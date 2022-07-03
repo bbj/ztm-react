b124e76d2fb0ee51eafbbee6d193e2f6ff4b09e4
Bruno Jullien <bruno.jullien@gmail.com>
2022-07-02 20:08:37 -0700

    42 - Monsters Rolodex - Storing Original Data

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     }
     console.log("constuctor");
   }
@@ -29,9 +30,31 @@ class App extends Component {
 
   render() {
     console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter(
+      (monster) => {
+        return monster.name.toLocaleLowerCase().includes(
+          this.state.searchField
+        );
+      }
+    );
+
     return (
       <div className="App" >
-        {this.state.monsters.map((monster) => {
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(
+              () => {
+                return { searchField };
+              }
+            );
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
